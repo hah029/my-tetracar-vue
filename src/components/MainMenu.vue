00781c0d84b12e0e943ef69144d90a45117a4900
Roman <roma.chekalin@gmail.com>
2026-03-18 22:07:15 +0300

    fix after merge

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,194 +1,187 @@
 <template>
     <div class="container">
-        <TransitionGroup 
-            name="buttons_group_showing"
-            tag="div"
-            class="buttons_group" 
-        >
-            <button 
-                v-for="(btn, index) in menuButtons" 
-                v-if="isMainMenuEnabled" 
-                :key="btn.id" 
-                class="menu_btn" 
-                :style="{ animationDelay: `${index * 0.06}s` }"
-                @click="btn.action"
-            >
+        <TransitionGroup name="buttons_group_showing" tag="div" class="buttons_group">
+            <button v-for="(btn, index) in menuButtons" v-if="isMainMenuEnabled" :key="btn.id" class="menu_btn"
+                :style="{ animationDelay: `${index * 0.06}s` }" @click="btn.action">
                 {{ btn.text }}
             </button>
         </TransitionGroup>
-        
-        <SettingsOverlay v-if="isSettingsEnabled" @event="handleEvent"/>
+
+        <SettingsOverlay v-if="isSettingsEnabled" @event="handleEvent" />
     </div>
 </template>
 
 
 <script setup lang="ts">
-    import { ref, defineEmits, onMounted } from "vue";
-    import { useGameState } from "@/store/gameState";
-    import SettingsOverlay from "./settings/SettingsOverlay.vue";
-    import { GameStates } from "@/game/core/GameState";
+import { ref, defineEmits, onMounted, computed } from "vue";
+import { useGameState } from "@/store/gameState";
+import SettingsOverlay from "./settings/SettingsOverlay.vue";
+import { GameStates } from "@/game/core/GameState";
+import { useTranslation } from "i18next-vue";
 
-    // подключаем store
-    const gameStore = useGameState();
+const { t } = useTranslation();
 
-    // подключаем emit
-    const emit = defineEmits(['event']);
+// подключаем store
+const gameStore = useGameState();
 
-    const isMainMenuEnabled = ref(false);
-    const isSettingsEnabled = ref(false);
+// подключаем emit
+const emit = defineEmits(['event']);
 
-    const menuButtons = [
-        // { id: 1, text: '- Старт -', action: startGame },
-        { id: 1, text: $t("mainMenu.startGame"), action: startGame },
-        { id: 2, text: '- Магазин -', action: null },
-        // { id: 3, text: '- Настройки -', action: goToSettings },
-        { id: 3, text: $t("mainMenu.settings"), action: goToSettings },
-        { id: 4, text: '- Рекорды -', action: null },
-    ];
+const isMainMenuEnabled = ref(false);
+const isSettingsEnabled = ref(false);
 
-    function startGame() {
-        emit('event', 'startGame');
-        gameStore.setState(GameStates.Countdown);
-    };
+const menuButtons = computed(() => [
+    // { id: 1, text: '- Старт -', action: startGame },
+    { id: 1, text: t("mainMenu.startGame"), action: startGame },
+    { id: 2, text: '- Магазин -', action: null },
+    // { id: 3, text: '- Настройки -', action: goToSettings },
+    { id: 3, text: t("mainMenu.settings"), action: goToSettings },
+    { id: 4, text: '- Рекорды -', action: null },
+]);
 
-    function goToSettings() {
-        isMainMenuEnabled.value = false;
-        setTimeout(() => {
-            isSettingsEnabled.value = true;
-        }, 300);
-    };
+function startGame() {
+    emit('event', 'startGame');
+    gameStore.setState(GameStates.Countdown);
+};
 
-    // ловим и обрабатываем события из дочерней компоненты SettingsOverlay.vue
-    function handleEvent(val_) {
-        if (val_ == 'goBackToMainMenu') {
-            isSettingsEnabled.value = false;
-            isMainMenuEnabled.value = true;
-            // setTimeout(() => {
-            //     isMainMenuEnabled.value = true;
-            // }, 300);
-        };
-    }  
+function goToSettings() {
+    isMainMenuEnabled.value = false;
+    setTimeout(() => {
+        isSettingsEnabled.value = true;
+    }, 300);
+};
 
-    onMounted(() => {
+// ловим и обрабатываем события из дочерней компоненты SettingsOverlay.vue
+function handleEvent(val_) {
+    if (val_ == 'goBackToMainMenu') {
+        isSettingsEnabled.value = false;
         isMainMenuEnabled.value = true;
-    });
+        // setTimeout(() => {
+        //     isMainMenuEnabled.value = true;
+        // }, 300);
+    };
+}
+
+onMounted(() => {
+    isMainMenuEnabled.value = true;
+});
 </script>
 
 
 <style lang="scss" scoped>
-    @use "@/styles/menu.scss";
-    @use "@/styles/animations.scss";
+@use "@/styles/menu.scss";
+@use "@/styles/animations.scss";
 
-    .buttons_group {
-        position: absolute;
-        bottom: 19.57%;
-        height: fit-content;
-        display: flex;
-        flex-direction: column;
-        background: none;
-        border: none;
+.buttons_group {
+    position: absolute;
+    bottom: 19.57%;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    background: none;
+    border: none;
 
-        // имитируем row-gap (между кнопками)
-        & > * + * {
-            margin-top: 1.56rem;
-        }
+    // имитируем row-gap (между кнопками)
+    &>*+* {
+        margin-top: 1.56rem;
     }
+}
 
-    .menu_btn {
-        background: none;
-        border: none;
-        // ---
-        font-family: 'vla_shu';
-        font-size: 2.25rem; // (36px)
-        color: #FDFFE3;
-        filter: drop-shadow(0 0 15px rgba(255, 246, 25, 0.4));
-        cursor: pointer;
+.menu_btn {
+    background: none;
+    border: none;
+    // ---
+    font-family: 'vla_shu';
+    font-size: 2.25rem; // (36px)
+    color: #FDFFE3;
+    filter: drop-shadow(0 0 15px rgba(255, 246, 25, 0.4));
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+
+    &:hover {
+        color: #72B3EE;
+        filter: drop-shadow(0 0 20px rgba(121, 190, 255, 1));
         transition: all 0.1s ease-in-out;
-        
+    }
+}
+
+
+
+
+.menu-title__mini {
+    font-size: 28px;
+    margin: 0;
+    margin-bottom: 10px;
+    text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
+    border-bottom: 1px solid white;
+}
+
+.menu-subtitle {
+    font-size: 54px;
+    margin: 0;
+    text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
+}
+
+
+
+.settings-btn-block {
+    &>.settings-btn {
+        background: none;
+        color: white;
+        border: none;
+        border-bottom: 1px solid #00000000;
+        padding: 10px 20px;
+        font-size: 18px;
+        cursor: pointer;
+        transition: all 0.3s ease;
+        cursor: pointer;
+
         &:hover {
-            color: #72B3EE;
-            filter: drop-shadow(0 0 20px rgba(121, 190, 255, 1));
-            transition: all 0.1s ease-in-out;
-        }
-    }
-
-
-
-
-    .menu-title__mini {
-        font-size: 28px;
-        margin: 0;
-        margin-bottom: 10px;
-        text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
-        border-bottom: 1px solid white;
-    }
-
-    .menu-subtitle {
-        font-size: 54px;
-        margin: 0;
-        text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
-    }
-
-    
-
-    .settings-btn-block {
-        &>.settings-btn {
-            background: none;
-            color: white;
-            border: none;
-            border-bottom: 1px solid #00000000;
-            padding: 10px 20px;
-            font-size: 18px;
-            cursor: pointer;
-            transition: all 0.3s ease;
-            cursor: pointer;
-
-            &:hover {
             /* background-color: rgba(255, 255, 255, 0.181); */
             border-bottom: 1px solid #ffffff;
             /* transform: scale(1.05); */
             animation: none;
             opacity: 0.75;
             text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
-            }
         }
     }
+}
 
-    .settings {
-        display: flex;
-        flex-direction: column;
-        gap: 25px;
-        margin: 40px 0;
+.settings {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    margin: 40px 0;
+}
+
+.settings-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 20px;
+}
+
+.toggle-btn {
+    background: none;
+    border: 1px solid white;
+    color: white;
+    padding: 6px 14px;
+    cursor: pointer;
+    transition: 0.1s;
+    width: 70px;
+
+    &:hover {
+        background: white;
+        color: black;
     }
+}
 
-    .settings-row {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        gap: 20px;
-    }
+.volume-row input {
+    flex: 1;
+}
 
-    .toggle-btn {
-        background: none;
-        border: 1px solid white;
-        color: white;
-        padding: 6px 14px;
-        cursor: pointer;
-        transition: 0.1s;
-        width: 70px;
-
-        &:hover {
-            background: white;
-            color: black;
-        }
-    }
-
-    .volume-row input {
-        flex: 1;
-    }
-
-    .volume-value {
-        width: 50px;
-        text-align: right;
-    }
+.volume-value {
+    width: 50px;
+    text-align: right;
+}
 </style>
--- src/components/settings/SettingsOverlay.vue
+++ src/components/settings/SettingsOverlay.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container correction">
         <div class="settings_container">
-            
+
             <div class="sub_container">
                 <Transition name="header_footer_block_anim">
                     <div v-if="isHeaderShown" class="header_block">
@@ -12,27 +12,18 @@
                     </div>
                 </Transition>
 
-                <TransitionGroup 
-                    name="buttons_group_showing"
-                    tag="div"
-                    class="buttons_group" 
-                >
-                    <button 
-                        v-for="(btn, index) in menuButtons" 
-                        v-if="isMainButtonsShown && settingsStateId==0"
-                        :key="btn.id" 
-                        class="menu_btn" 
-                        :style="{ animationDelay: `${index * 0.06}s` }"
-                        @click="btn.action"
-                    >
+                <TransitionGroup name="buttons_group_showing" tag="div" class="buttons_group">
+                    <button v-for="(btn, index) in menuButtons" v-if="isMainButtonsShown && settingsStateId == 0"
+                        :key="btn.id" class="menu_btn" :style="{ animationDelay: `${index * 0.06}s` }"
+                        @click="btn.action">
                         {{ btn.text }}
                     </button>
-                    <div v-else-if="isMainButtonsShown && settingsStateId==1" class="asd">
+                    <div v-else-if="isMainButtonsShown && settingsStateId == 1" class="asd">
                         <SoundSettings />
                     </div>
-                    <div v-else-if="isMainButtonsShown && settingsStateId==2" class="asd">Язык...</div>
-                    <div v-else-if="isMainButtonsShown && settingsStateId==3" class="asd">Управление...</div>
-                    <div v-else-if="isMainButtonsShown && settingsStateId==4" class="asd">
+                    <div v-else-if="isMainButtonsShown && settingsStateId == 2" class="asd">Язык...</div>
+                    <div v-else-if="isMainButtonsShown && settingsStateId == 3" class="asd">Управление...</div>
+                    <div v-else-if="isMainButtonsShown && settingsStateId == 4" class="asd">
                         <DebugSettings />
                     </div>
                 </TransitionGroup>
@@ -40,7 +31,8 @@
             </div>
 
             <Transition name="header_footer_block_anim">
-                <button v-if="isBackButtonsShown" class="menu_btn" @click="backButtonClick()">{{ $t("mainMenu.goBack") }}</button>
+                <button v-if="isBackButtonsShown" class="menu_btn" @click="backButtonClick()">{{ $t("mainMenu.goBack")
+                }}</button>
             </Transition>
         </div>
     </div>
@@ -48,239 +40,246 @@
 
 
 <script setup lang="ts">
-    import DebugSettings from "./DebugSettings.vue";
-    import SoundSettings from "./SoundSettings.vue";
-    import LanguageSettings from "./LanguageSettings.vue";
-    import { onMounted, computed, defineEmits, ref } from "vue";
-    import { useTranslation } from "i18next-vue";
+import DebugSettings from "./DebugSettings.vue";
+import SoundSettings from "./SoundSettings.vue";
+import LanguageSettings from "./LanguageSettings.vue";
+import { onMounted, computed, defineEmits, ref } from "vue";
+import { useTranslation } from "i18next-vue";
 
-    const { t } = useTranslation();
+const { t } = useTranslation();
 
-    // подключаем emit
-    const emit = defineEmits(['event']);
+// подключаем emit
+const emit = defineEmits(['event']);
 
-    const settingsType = ref<'controls' | 'sound' | 'graphic' | 'debug' | 'lang'>("sound")
-    const isHeaderShown = ref(false);
+const settingsType = ref<'controls' | 'sound' | 'graphic' | 'debug' | 'lang'>("sound")
+const isHeaderShown = ref(false);
 
-    const isMainButtonsShown = ref(false);
-    // const isGraphicsButtonsShown = ref(false);
+const isMainButtonsShown = ref(false);
+// const isGraphicsButtonsShown = ref(false);
 
-    const isBackButtonsShown = ref(false);
+const isBackButtonsShown = ref(false);
 
-    const settingsStateId = ref(0);
+const settingsStateId = ref(0);
 
-    // const dynamicTitleNameDefault = ref('Настройки');
-    const dynamicTitleNameDefault = ref($t("settings.title"));
-    const dynamicTitleName = ref('Настройки');
+// const dynamicTitleNameDefault = ref('Настройки');
+const dynamicTitleNameDefault = ref(t("settings.title"));
+const dynamicTitleName = ref('Настройки');
 
-    const menuButtons = [
-        { id: 1, text: '- Графика и аудио -', action: switchToGraphics },
-        { id: 2, text: '- Язык -', action: switchToLanguage },
-        { id: 3, text: '- Управление -', action: switchToControls },
-        { id: 4, text: '- Об игре -', action: switchToAbout },
-    ];
+const menuButtons = [
+    { id: 1, text: '- Графика и аудио -', action: switchToGraphics },
+    { id: 2, text: '- Язык -', action: switchToLanguage },
+    { id: 3, text: '- Управление -', action: switchToControls },
+    { id: 4, text: '- Об игре -', action: switchToAbout },
+];
 
-    const menuList = computed(() => ({
-        controls: { name: t("settings.settingsMenuList.controls"), comp: null },
-        sound: { name: t("settings.settingsMenuList.sound"), comp: SoundSettings },
-        graphic: { name: t("settings.settingsMenuList.graphic"), comp: null },
-        debug: { name: t("settings.settingsMenuList.debug"), comp: DebugSettings },
-        lang: { name: t("settings.settingsMenuList.lang"), comp: LanguageSettings },
-    }));
+// const menuList = computed(() => ({
+//     controls: { name: t("settings.settingsMenuList.controls"), comp: null },
+//     sound: { name: t("settings.settingsMenuList.sound"), comp: SoundSettings },
+//     graphic: { name: t("settings.settingsMenuList.graphic"), comp: null },
+//     debug: { name: t("settings.settingsMenuList.debug"), comp: DebugSettings },
+//     lang: { name: t("settings.settingsMenuList.lang"), comp: LanguageSettings },
+// }));
 
-    const getSettingsComponent = computed(() => menuList.value[settingsType.value]?.comp || null);
+// const getSettingsComponent = computed(() => menuList.value[settingsType.value]?.comp || null);
 
-    function switchToGraphics() {
-        settingsStateId.value = 1;
-        dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
-    };
-    function switchToLanguage() {
-        settingsStateId.value = 2;
-        dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
-    };
-    function switchToControls() {
-        settingsStateId.value = 3;
-        dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
-    };
-    function switchToAbout() {
-        settingsStateId.value = 4;
-        dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
-    };
+function switchToGraphics() {
+    settingsStateId.value = 1;
+    dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
+};
+function switchToLanguage() {
+    settingsStateId.value = 2;
+    dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
+};
+function switchToControls() {
+    settingsStateId.value = 3;
+    dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
+};
+function switchToAbout() {
+    settingsStateId.value = 4;
+    dynamicTitleName.value = menuButtons[settingsStateId.value - 1].text;
+};
 
-    function backButtonClick() {
-        if (settingsStateId.value == 0) {
-            isHeaderShown.value = false;
-            setTimeout(() => {
-                isMainButtonsShown.value = false;
-            }, 100);
-            setTimeout(() => {
-                isBackButtonsShown.value = false;
-            }, 400);
-    
-            setTimeout(() => {
-                emit('event', 'goBackToMainMenu');
-            }, 500);
+function backButtonClick() {
+    if (settingsStateId.value == 0) {
+        isHeaderShown.value = false;
+        setTimeout(() => {
+            isMainButtonsShown.value = false;
+        }, 100);
+        setTimeout(() => {
+            isBackButtonsShown.value = false;
+        }, 400);
 
-        } else {
-            settingsStateId.value = 0;
-            dynamicTitleName.value = dynamicTitleNameDefault.value;
-        };
-    };
+        setTimeout(() => {
+            emit('event', 'goBackToMainMenu');
+        }, 500);
 
-    onMounted(() => {
+    } else {
         settingsStateId.value = 0;
         dynamicTitleName.value = dynamicTitleNameDefault.value;
-        isHeaderShown.value = true;
-        setTimeout(() => {
-            isMainButtonsShown.value = true;
-        }, 200);
-        setTimeout(() => {
-            isBackButtonsShown.value = true;
-        }, 500);
-    });
+    };
+};
+
+onMounted(() => {
+    settingsStateId.value = 0;
+    dynamicTitleName.value = dynamicTitleNameDefault.value;
+    isHeaderShown.value = true;
+    setTimeout(() => {
+        isMainButtonsShown.value = true;
+    }, 200);
+    setTimeout(() => {
+        isBackButtonsShown.value = true;
+    }, 500);
+});
 </script>
 
 
 <style scoped lang="scss">
-    @use "@/styles/menu.scss";
-    @use "@/styles/animations.scss";
+@use "@/styles/menu.scss";
+@use "@/styles/animations.scss";
 
-    .correction {
-        justify-content: flex-end !important;
-    }
-    .settings_container {
-        height: 30rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-        box-sizing: border-box;
-        padding-bottom: 2.687rem;
-    }
-    .sub_container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-    }
+.correction {
+    justify-content: flex-end !important;
+}
 
-    // #region - header
-    .header_block {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-end;
+.settings_container {
+    height: 30rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+    padding-bottom: 2.687rem;
+}
+
+.sub_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+// #region - header
+.header_block {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+}
+
+.header_text {
+    font-family: 'vla_shu';
+    font-size: 2.5rem; // (40px)
+    color: #72B3EE;
+    margin-bottom: 1rem;
+    line-height: 1;
+}
+
+.header_image {
+    width: 34.625rem;
+}
+
+.image {
+    filter: invert(86%) sepia(40%) saturate(5593%) hue-rotate(183deg) brightness(104%) contrast(101%);
+}
+
+// #endregion
+
+.buttons_group {
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    background: none;
+    border: none;
+    margin-top: 2.4rem;
+
+    // имитируем row-gap (между кнопками)
+    &>*+* {
+        margin-top: 0.938rem;
     }
-    .header_text {
-        font-family: 'vla_shu';
-        font-size: 2.5rem; // (40px)
+}
+
+.menu_btn {
+    background: none;
+    border: none;
+    // ---
+    font-family: 'vla_shu';
+    font-size: 1.875rem; // (30px)
+    color: #FDFFE3;
+    filter: drop-shadow(0 0 15px rgba(255, 246, 25, 0.4));
+    cursor: pointer;
+    transition: all 0.1s ease-in-out;
+
+    &:hover {
         color: #72B3EE;
-        margin-bottom: 1rem;
-        line-height: 1;
-    }
-    .header_image {
-        width: 34.625rem;
-    }
-    .image {
-        filter: invert(86%) sepia(40%) saturate(5593%) hue-rotate(183deg) brightness(104%) contrast(101%);
-    }
-    // #endregion
-
-    .buttons_group {
-        height: fit-content;
-        display: flex;
-        flex-direction: column;
-        background: none;
-        border: none;
-        margin-top: 2.4rem;
-
-        // имитируем row-gap (между кнопками)
-        & > * + * {
-            margin-top: 0.938rem;
-        }
-    }
-    .menu_btn {
-        background: none;
-        border: none;
-        // ---
-        font-family: 'vla_shu';
-        font-size: 1.875rem; // (30px)
-        color: #FDFFE3;
-        filter: drop-shadow(0 0 15px rgba(255, 246, 25, 0.4));
-        cursor: pointer;
+        filter: drop-shadow(0 0 20px rgba(121, 190, 255, 1));
         transition: all 0.1s ease-in-out;
-        
-        &:hover {
-            color: #72B3EE;
-            filter: drop-shadow(0 0 20px rgba(121, 190, 255, 1));
-            transition: all 0.1s ease-in-out;
-        }
     }
+}
 
-    // .back_button {
-    //     // margin-bottom: 0;
-    //     margin-top: 1rem;
-    // }
-
-
-
-
-    .settings-overlay {
-        display: flex;
-        justify-content: flex-start;
-        gap: 60px;
-        // border: 1px solid red;
-        padding: 50px;
-    }
-
-    .settings-inner-overlay {
-        min-width: 20rem;
-        min-height: 10rem;
-    }
-
-    .settings-menu-list {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        width: 10rem;
-        min-width: 100px;
-        border-right: 1px solid rgb(255, 255, 255, 0.1);
-
-        &>li {
-            height: 50px;
-            line-height: 50px;
-            text-transform: uppercase;
-            cursor: pointer;
-            transition: all 300ms;
-            text-align: center;
-            vertical-align: center;
-
-            &:hover {
-                background-color: rgb(255, 255, 255, 0.5);
-            }
-
-            &.selected {
-                background-color: rgb(255, 255, 255, 0.2);
-            }
-        }
-    }
+// .back_button {
+//     // margin-bottom: 0;
+//     margin-top: 1rem;
+// }
 
 
-    .menu-subtitle {
-        font-size: 54px;
+
+
+.settings-overlay {
+    display: flex;
+    justify-content: flex-start;
+    gap: 60px;
+    // border: 1px solid red;
+    padding: 50px;
+}
+
+.settings-inner-overlay {
+    min-width: 20rem;
+    min-height: 10rem;
+}
+
+.settings-menu-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 10rem;
+    min-width: 100px;
+    border-right: 1px solid rgb(255, 255, 255, 0.1);
+
+    &>li {
+        height: 50px;
+        line-height: 50px;
         text-transform: uppercase;
-        margin: 0 0 30px 0;
-        text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
+        cursor: pointer;
+        transition: all 300ms;
+        text-align: center;
+        vertical-align: center;
+
+        &:hover {
+            background-color: rgb(255, 255, 255, 0.5);
+        }
+
+        &.selected {
+            background-color: rgb(255, 255, 255, 0.2);
+        }
     }
+}
 
 
-    /* we will explain what these classes do next! */
-    .v-enter-active {
-        transition: opacity 0.5s ease;
-    }
+.menu-subtitle {
+    font-size: 54px;
+    text-transform: uppercase;
+    margin: 0 0 30px 0;
+    text-shadow: 0 0 20px rgba(0, 255, 255, 0.741);
+}
 
-    .v-enter-from,
-    .v-leave-to {
-        opacity: 0;
-    }
+
+/* we will explain what these classes do next! */
+.v-enter-active {
+    transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+    opacity: 0;
+}
 </style>
