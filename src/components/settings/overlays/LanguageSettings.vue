c2bedfdd27089df89f8956bda6824371461ad90c
Tikhomolov, Konstantin <k3incho@inbox.ru>
2026-03-30 18:08:32 +0300

    Доработки по флагам стран

--- src/components/settings/overlays/LanguageSettings.vue
+++ src/components/settings/overlays/LanguageSettings.vue
@@ -11,15 +11,15 @@
                         </div>
                         <div class="flag_inner_container">
                             <img class="flag_frame" :class="{ 'flag_frame--active': selectedLang === lang.code }"
-                                src="@/assets/images/flag_frame.svg" />
+                                :src="langSrc.frame" />
                         </div>
                     </div>
                     <div class="addit_font" :class="{ 'addit_font--active': selectedLang === lang.code }">
-                        {{ lang.name }}
+                        {{ writeLangName(lang) }}
                     </div>
                 </div>
                 <div class="checker_image_container">
-                    <img v-if="selectedLang === lang.code" class="checker_img" src="@/assets/images/flag_checker.svg" />
+                    <img v-if="selectedLang === lang.code" class="checker_img" src="@/assets/images/checker.svg" />
                 </div>
             </label>
             <input :checked="selectedLang === lang.code" type="radio" :name="lang.name" :id="lang.code"
@@ -28,138 +28,151 @@
     </TransitionGroup>
 </template>
 
+
 <script setup lang="ts">
-import { onMounted, ref, watch, defineProps, computed } from "vue";
-import { useTranslation } from "i18next-vue";
-import { createNewText } from '@/helpers/functions';
-import { langSrc } from "@/locales";
-import { resolveAutoLanguage, uiLanguages } from "@/helpers/i18n";
+    import { onMounted, ref, watch, defineProps, computed } from "vue";
+    import { useTranslation } from "i18next-vue";
+    import { createNewText } from '@/helpers/functions';
+    import { langSrc } from "@/locales";
+    import { resolveAutoLanguage, uiLanguages } from "@/helpers/i18n";
 
-const { i18next } = useTranslation();
+    const { i18next } = useTranslation();
 
-const foo = createNewText();
-const rowView = ref(false);
+    const foo = createNewText();
+    const rowView = ref(false);
 
-// 👉 что выбрал пользователь (auto | ru | en)
-const selectedLang = ref<string>(localStorage.getItem('lang') || 'auto');
+    // 👉 что выбрал пользователь (auto | ru | en)
+    const selectedLang = ref<string>(localStorage.getItem('lang') || 'auto');
 
-// 👉 языки для UI (включая auto)
+    // 👉 языки для UI (включая auto)
 
 
-const availableLanguages = computed(() => {
-    return uiLanguages.map((code) => ({
-        code,
-        name: foo.makeText(`settings.language.${code}`, 'empty'),
-        src: langSrc[code],
-    }));
-});
+    const availableLanguages = computed(() => {
+        return uiLanguages.map((code) => ({
+            code,
+            name: foo.makeText(`settings.language.${code}`, 'empty'),
+            src: langSrc[code],
+        }));
+    });
 
-const props = defineProps<{
-    backStatus: boolean;
-}>();
+    const props = defineProps<{
+        backStatus: boolean;
+    }>();
 
-watch(() => props.backStatus, (newVal) => {
-    if (newVal) {
-        rowView.value = false;
-    }
-});
-
-// 🎯 установка языка
-function setLanguage(code: string) {
-    selectedLang.value = code;
-    localStorage.setItem('lang', code);
-
-    if (code === "auto") {
-        i18next.changeLanguage(resolveAutoLanguage());
-    } else {
-        i18next.changeLanguage(code);
-    }
-}
-
-// 🚀 init
-onMounted(() => {
-    window.addEventListener('languagechange', () => {
-        if (selectedLang.value === 'auto') {
-            const lang = resolveAutoLanguage();
-            i18next.changeLanguage(lang);
+    watch(() => props.backStatus, (newVal) => {
+        if (newVal) {
+            rowView.value = false;
         }
     });
 
-    setTimeout(() => {
-        rowView.value = true;
-    }, 400);
-});
+    // 🎯 установка языка
+    function setLanguage(code: string) {
+        selectedLang.value = code;
+        localStorage.setItem('lang', code);
+
+        if (code === "auto") {
+            i18next.changeLanguage(resolveAutoLanguage());
+        } else {
+            i18next.changeLanguage(code);
+        }
+    };
+
+    function writeLangName(lang_) {
+        if (lang_.code == 'auto') {
+            return lang_.name + ' (' + resolveAutoLanguage() + ')';
+        } else {
+            return lang_.name;
+        };
+    };
+
+    // 🚀 init
+    onMounted(() => {
+        window.addEventListener('languagechange', () => {
+            if (selectedLang.value === 'auto') {
+                const lang = resolveAutoLanguage();
+                i18next.changeLanguage(lang);
+            }
+        });
+
+        setTimeout(() => {
+            rowView.value = true;
+        }, 400);
+    });
 </script>
 
+
 <style lang="scss">
-@use "@/styles/menu.scss";
-@use "@/styles/settings.scss";
-@use "@/styles/animations.scss";
+    @use "@/styles/menu.scss";
+    @use "@/styles/settings.scss";
+    @use "@/styles/animations.scss";
 
-.container_correction {
-    margin-top: 0;
-}
-
-.row_correction {
-    padding: 0.625rem;
-    background: none;
-    border: none;
-    cursor: pointer;
-
-    &:hover {
-        background-color: rgba(253, 255, 227, 0.3);
+    .container_correction {
+        margin-top: 0;
     }
-}
 
-.left_part {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    background: none;
-    gap: 0.9375rem;
-    height: 100%;
-}
+    .row_correction {
+        padding: 0.625rem;
+        background: none;
+        border: none;
+        cursor: pointer;
 
-.checker_image_container {
-    height: 1.875rem;
-}
+        &:hover {
+            background-color: rgba(253, 255, 227, 0.3);
+        }
+    }
 
-.flag_image_container {
-    position: relative;
-    width: 2.625rem;
-    height: 1.875rem;
-}
+    .left_part {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        background: none;
+        gap: 0.9375rem;
+        height: 100%;
+    }
 
-.flag_inner_container {
-    position: absolute;
-    width: 100%;
-}
+    .checker_image_container {
+        height: 1.875rem;
+    }
 
-.flag_img {
-    width: 100%;
-}
+    .flag_image_container {
+        position: relative;
+        width: 2.625rem;
+        height: 1.875rem;
+    }
 
-.flag_frame {
-    width: 100%;
-    filter: invert(92%) sepia(19%) saturate(274%) hue-rotate(26deg) brightness(107%) contrast(105%);
-}
+    .flag_inner_container {
+        position: absolute;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
 
-.flag_frame--active {
-    filter: invert(75%) sepia(7%) saturate(4910%) hue-rotate(179deg) brightness(96%) contrast(94%);
-}
+    .flag_img {
+        margin-top: 0.3rem;
+        width: 75%;
+        border-radius: 0.125rem;
+    }
 
-.checker_img {
-    width: 90%;
-    height: 90%;
-    filter: invert(75%) sepia(7%) saturate(4910%) hue-rotate(179deg) brightness(96%) contrast(94%);
-}
+    .flag_frame {
+        width: 100%;
+        filter: invert(92%) sepia(19%) saturate(274%) hue-rotate(26deg) brightness(107%) contrast(105%);
+    }
 
-input[type="radio"] {
-    display: none;
-}
+    .flag_frame--active {
+        filter: invert(75%) sepia(7%) saturate(4910%) hue-rotate(179deg) brightness(96%) contrast(94%);
+    }
 
-.addit_font--active {
-    color: #72B3EE;
-}
+    .checker_img {
+        width: 90%;
+        height: 90%;
+        filter: invert(75%) sepia(7%) saturate(4910%) hue-rotate(179deg) brightness(96%) contrast(94%);
+    }
+
+    input[type="radio"] {
+        display: none;
+    }
+
+    .addit_font--active {
+        color: #72B3EE;
+    }
 </style>
-```
--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,10 +1,17 @@
 import ru from "./ru/translation.json";
 import en from "./en/translation.json";
 
-import flag_auto from "@/assets/images/flag_auto.svg";
-import flag_ru from "@/assets/images/flag_ru.svg";
-import flag_en from "@/assets/images/flag_en.svg";
-import flag_es from "@/assets/images/flag_es.svg";
+import flag_frame from "@/assets/images/flags/flag_frame.svg";          // Рамка для флага страны (универсальная)
+import flag_auto from "@/assets/images/flags/flag_auto.svg";            // Значок автоматического определения языка (auto)
+import flag_ru from "@/assets/images/flags/flag_ru.svg";                // Русский
+
+// import flag_en_brit from "@/assets/images/flags/flag_pt-BR.svg";   
+
+import flag_en_brit from "@/assets/images/flags/flag_en_british.svg";   // Английский (Британский флаг)
+// import flag_en_usa from "@/assets/images/flags/flag_en_usa.svg";        // Английский (флаг США)
+// import flag_es from "@/assets/images/flags/flag_es.svg";                // Испанский
+// import flag_zh from "@/assets/images/flags/flag_zh.svg";                // Китайский
+// import flag_zh from "@/assets/images/flags/flag_pt-BR.svg";                // Португальский (Бразильский)
 
 export const locales = {
   en: en,
@@ -12,8 +19,12 @@ export const locales = {
 };
 
 export const langSrc: Record<string, string> = {
+  frame: flag_frame,
   auto: flag_auto,
   ru: flag_ru,
-  en: flag_en,
-  es: flag_es,
+  en: flag_en_brit,
+//   en: flag_en_usa,
+//   es: flag_es,
+//   es: flag_zh,
+//   es: flag_pt-BR,
 };
