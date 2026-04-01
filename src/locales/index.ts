3bb9464cf2934a67e6ff371f9a5ca6d83c7d6d0b
K2INcHO <k3incho@inbox.ru>
2026-04-01 08:57:12 +0300

    Правки по UI

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,15 +3,20 @@ import en from "./en/translation.json";
 
 import flag_frame from "@/assets/images/flags/flag_frame.svg";          // Рамка для флага страны (универсальная)
 import flag_auto from "@/assets/images/flags/flag_auto.svg";            // Значок автоматического определения языка (auto)
-import flag_ru from "@/assets/images/flags/flag_ru.svg";                // Русский
+import flag_ru from "@/assets/images/flags/flag_ru.svg";                // Россия
 
-// import flag_en_brit from "@/assets/images/flags/flag_pt-BR.svg";   
+import flag_en_brit from "@/assets/images/flags/flag_en_brit.svg";   
 
-import flag_en_brit from "@/assets/images/flags/flag_en_british.svg";   // Английский (Британский флаг)
+// import flag_en_brit from "@/assets/images/flags/flag_en_brit.svg";      // Английский (Британский флаг)
 // import flag_en_usa from "@/assets/images/flags/flag_en_usa.svg";        // Английский (флаг США)
-// import flag_es from "@/assets/images/flags/flag_es.svg";                // Испанский
-// import flag_zh from "@/assets/images/flags/flag_zh.svg";                // Китайский
-// import flag_zh from "@/assets/images/flags/flag_pt-BR.svg";                // Португальский (Бразильский)
+// import flag_de from "@/assets/images/flags/flag_de.svg";                // Германия
+// import flag_es from "@/assets/images/flags/flag_es.svg";                // Испания
+// import flag_zh from "@/assets/images/flags/flag_zh.svg";                // Китай
+// import flag_pt_BR from "@/assets/images/flags/flag_pt_BR.svg";          // Португалия (Бразильский)
+// import flag_ja from "@/assets/images/flags/flag_ja.svg";                // Япония
+// import flag_ko from "@/assets/images/flags/flag_ko.svg";                // Южная Корея
+// import flag_tr from "@/assets/images/flags/flag_tr.svg";                // Турция
+// import flag_fr from "@/assets/images/flags/flag_fr.svg";                // Франция
 
 export const locales = {
   en: en,
@@ -23,8 +28,13 @@ export const langSrc: Record<string, string> = {
   auto: flag_auto,
   ru: flag_ru,
   en: flag_en_brit,
-//   en: flag_en_usa,
-//   es: flag_es,
-//   es: flag_zh,
-//   es: flag_pt-BR,
+  // en: flag_en_usa,
+  // de: flag_de,
+  // es: flag_es,
+  // zh: flag_zh,
+  // pt: flag_pt_BR,   // pg два (Европ и Бразил)
+  // ja: flag_ja,
+  // ko: flag_ko,
+  // tr: flag_tr,
+  // fr: flag_fr,
 };
