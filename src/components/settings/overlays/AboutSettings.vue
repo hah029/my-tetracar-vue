b71b15778507f15a821e71ddedafa3954eb7a8fb
Roman <roma.chekalin@gmail.com>
2026-04-26 15:10:28 +0300

    fix npm warnings

--- src/components/settings/overlays/AboutSettings.vue
+++ src/components/settings/overlays/AboutSettings.vue
@@ -1,13 +1,7 @@
 <template>
     <TransitionGroup name="buttons_group_showing" tag="div" class="settings_sub_container group_correction">
-        <button 
-            v-for="(btn, index) in aboutButtons" 
-            v-if="isRowsEnabled"
-            key="btn.id" 
-            class="menu_btn btn_correction"
-            :style="{ animationDelay: `${index * 0.06}s` }" 
-            @click="btn.action"
-        >
+        <button v-for="(btn, index) in aboutButtons" v-if="isRowsEnabled" key="btn.id" class="menu_btn btn_correction"
+            :style="{ animationDelay: `${index * 0.06}s` }" @click="btn.action">
             {{ btn.text }}
         </button>
     </TransitionGroup>
@@ -15,62 +9,62 @@
 
 
 <script setup lang="ts">
-    import { onMounted, ref, watch, defineProps, computed } from "vue";
-    import { createNewText } from '@/helpers/functions';
+import { onMounted, ref, watch, computed } from "vue";
+import { createNewText } from '@/helpers/functions';
 
-    const foo = createNewText();
-    const isRowsEnabled = ref(false);
+const foo = createNewText();
+const isRowsEnabled = ref(false);
 
-    const aboutButtons = computed(() => [
-        // { id: 1, text: foo.makeText("settings.about.eula"), action: goToEula },
-        { id: 1, text: foo.makeText("settings.about.eula"), action: null },
-        { id: 2, text: foo.makeText("settings.about.thirdParty"), action: null },
-        { id: 3, text: foo.makeText("settings.about.aboutUs"), action: null },
-    ]);
+const aboutButtons = computed(() => [
+    // { id: 1, text: foo.makeText("settings.about.eula"), action: goToEula },
+    { id: 1, text: foo.makeText("settings.about.eula"), action: null },
+    { id: 2, text: foo.makeText("settings.about.thirdParty"), action: null },
+    { id: 3, text: foo.makeText("settings.about.aboutUs"), action: null },
+]);
 
-    // function goToEula() {
-    //     gameStore.setState(GameStates.Countdown);
-    // };
+// function goToEula() {
+//     gameStore.setState(GameStates.Countdown);
+// };
 
-    // function goToSettings() {
-    //     isRowsEnabled.value = false;
-    //     setTimeout(() => {
-    //         gameStore.openSettings();
-    //     }, 300);
-    // };
+// function goToSettings() {
+//     isRowsEnabled.value = false;
+//     setTimeout(() => {
+//         gameStore.openSettings();
+//     }, 300);
+// };
 
-    const props = defineProps<{
-        backStatus: boolean;
-    }>();
+const props = defineProps<{
+    backStatus: boolean;
+}>();
 
-    watch(() => props.backStatus, (newVal) => {
-        if (newVal) {
-            isRowsEnabled.value = false;
-        };
-    });
+watch(() => props.backStatus, (newVal) => {
+    if (newVal) {
+        isRowsEnabled.value = false;
+    };
+});
 
-    onMounted(() => {
-        setTimeout(() => {
-            isRowsEnabled.value = true;
-        }, 400);
-    });
+onMounted(() => {
+    setTimeout(() => {
+        isRowsEnabled.value = true;
+    }, 400);
+});
 </script>
 
 
 <style lang="scss" scoped>
-    @use "@/styles/menu.scss";
-    @use "@/styles/animations.scss";
+@use "@/styles/menu.scss";
+@use "@/styles/animations.scss";
 
-    .group_correction {
-        bottom: 19.57%;
-        width: max-content;
+.group_correction {
+    bottom: 19.57%;
+    width: max-content;
 
-        &>*+* {
-            margin-top: 0.9375rem; // 15px - row-gap (между кнопками)
-        }
+    &>*+* {
+        margin-top: 0.9375rem; // 15px - row-gap (между кнопками)
     }
+}
 
-    .btn_correction {
-        font-size: 1.875rem; // (30px)
-    }
+.btn_correction {
+    font-size: 1.875rem; // (30px)
+}
 </style>
--- src/components/settings/overlays/SoundSettings.vue
+++ src/components/settings/overlays/SoundSettings.vue
@@ -3,19 +3,19 @@
         <div v-if="rowView[0]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.vfxEnabled", "empty") }}</span>
             <button class="toggle_btn" :class="{ 'toggle_btn--active': vfxTempStore }" @click="toggleVfx">
-                {{ vfxTempStore ? 
-                    foo.makeText("settings.toggleOn", "empty") : 
-                    foo.makeText("settings.toggleOff", "empty") 
+                {{ vfxTempStore ?
+                    foo.makeText("settings.toggleOn", "empty") :
+                    foo.makeText("settings.toggleOff", "empty")
                 }}
             </button>
         </div>
 
         <div v-if="rowView[1]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.musicEnabled", "empty") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': audioStore.musicEnabled }" @click="toggleMusic" >
-                {{ audioStore.musicEnabled ? 
-                    foo.makeText("settings.toggleOn", "empty") : 
-                    foo.makeText("settings.toggleOff", "empty") 
+            <button class="toggle_btn" :class="{ 'toggle_btn--active': audioStore.musicEnabled }" @click="toggleMusic">
+                {{ audioStore.musicEnabled ?
+                    foo.makeText("settings.toggleOn", "empty") :
+                    foo.makeText("settings.toggleOff", "empty")
                 }}
             </button>
         </div>
@@ -23,120 +23,115 @@
         <div v-if="rowView[2]" class="settings_row">
             <span>{{ $t("settings.vfxAndMusic.sfxEnabled") }}</span>
             <button class="toggle_btn" :class="{ 'toggle_btn--active': audioStore.sfxEnabled }" @click="toggleSound">
-                {{ audioStore.sfxEnabled ? 
-                    foo.makeText("settings.toggleOn", "empty") : 
-                    foo.makeText("settings.toggleOff", "empty") 
+                {{ audioStore.sfxEnabled ?
+                    foo.makeText("settings.toggleOn", "empty") :
+                    foo.makeText("settings.toggleOff", "empty")
                 }}
             </button>
         </div>
 
         <div v-if="rowView[3]" class="settings_row">
             <span>{{ $t("settings.vfxAndMusic.volumeLevel") }}</span>
-            <input 
-                type="range" 
-                min="0" max="1" step="0.01" 
-                v-model="volume" 
-                @input="updateVolume" 
-                class="custom_slider"
-            />
+            <input type="range" min="0" max="1" step="0.01" v-model="volume" @input="updateVolume"
+                class="custom_slider" />
         </div>
     </TransitionGroup>
 </template>
 
 
 <script setup lang="ts">
-    import { onMounted, defineProps, watch, ref } from "vue";
-    import { useAudioStore } from "@/store/audioStore";
-    import { SoundManager } from "@/game/sound/SoundManager";
-    import { createNewText } from '@/helpers/functions';
+import { onMounted, watch, ref } from "vue";
+import { useAudioStore } from "@/store/audioStore";
+import { SoundManager } from "@/game/sound/SoundManager";
+import { createNewText } from '@/helpers/functions';
 
-    const audioStore = useAudioStore();
-    const soundManager = SoundManager.getInstance();
-    const volume = ref(Number(localStorage.getItem("masterVolume") ?? 0.6));
-    const rowView = ref([false, false, false, false]);
-    const vfxTempStore = ref(false);
+const audioStore = useAudioStore();
+const soundManager = SoundManager.getInstance();
+const volume = ref(Number(localStorage.getItem("masterVolume") ?? 0.6));
+const rowView = ref([false, false, false, false]);
+const vfxTempStore = ref(false);
 
-    const foo = createNewText();
-    
-    function toggleVfx() {
-        console.log('Включаем / выключаем графические эффекты...');
-        vfxTempStore.value = !vfxTempStore.value;
-        // audioStore.toggleMusic();
-        // if (audioStore.vfxEnabled) soundManager.play("music_background");
-    };
+const foo = createNewText();
 
-    function toggleMusic() {
-        audioStore.toggleMusic();
-        if (audioStore.musicEnabled) soundManager.play("music_background");
-    };
+function toggleVfx() {
+    console.log('Включаем / выключаем графические эффекты...');
+    vfxTempStore.value = !vfxTempStore.value;
+    // audioStore.toggleMusic();
+    // if (audioStore.vfxEnabled) soundManager.play("music_background");
+};
 
-    function toggleSound() {
-        audioStore.toggleSFX();
-    };
+function toggleMusic() {
+    audioStore.toggleMusic();
+    if (audioStore.musicEnabled) soundManager.play("music_background");
+};
 
-    function updateVolume() {
-        soundManager.setMasterVolume(volume.value);
-        localStorage.setItem("masterVolume", volume.value.toString());
-        soundManager.play("sfx_click");
-    };
+function toggleSound() {
+    audioStore.toggleSFX();
+};
 
-    const props = defineProps<{
-        backStatus: boolean;
-    }>();
+function updateVolume() {
+    soundManager.setMasterVolume(volume.value);
+    localStorage.setItem("masterVolume", volume.value.toString());
+    soundManager.play("sfx_click");
+};
 
-    watch(() => props.backStatus, (newVal) => {
-        if (newVal) {
-            rowView.value[0] = false;
-            setTimeout(() => {
-                rowView.value[1] = false;
-            }, 100);
-            setTimeout(() => {
-                rowView.value[2] = false;
-            }, 200);
-            setTimeout(() => {
-                rowView.value[3] = false;
-            }, 300);
-        };
-    });
+const props = defineProps<{
+    backStatus: boolean;
+}>();
 
-    onMounted(() => {
+watch(() => props.backStatus, (newVal) => {
+    if (newVal) {
+        rowView.value[0] = false;
         setTimeout(() => {
-            rowView.value[0] = true;
-            setTimeout(() => {
-                rowView.value[1] = true;
-            }, 100);
-            setTimeout(() => {
-                rowView.value[2] = true;
-            }, 200);
-            setTimeout(() => {
-                rowView.value[3] = true;
-            }, 300);
-        }, 400);
-    });
+            rowView.value[1] = false;
+        }, 100);
+        setTimeout(() => {
+            rowView.value[2] = false;
+        }, 200);
+        setTimeout(() => {
+            rowView.value[3] = false;
+        }, 300);
+    };
+});
+
+onMounted(() => {
+    setTimeout(() => {
+        rowView.value[0] = true;
+        setTimeout(() => {
+            rowView.value[1] = true;
+        }, 100);
+        setTimeout(() => {
+            rowView.value[2] = true;
+        }, 200);
+        setTimeout(() => {
+            rowView.value[3] = true;
+        }, 300);
+    }, 400);
+});
 </script>
 
 
 <style scoped lang="scss">
-    @use "@/styles/menu.scss";
-    @use "@/styles/settings.scss";
-    @use "@/styles/animations.scss";
+@use "@/styles/menu.scss";
+@use "@/styles/settings.scss";
+@use "@/styles/animations.scss";
 
-    // стили трека
-    .custom_slider {
-        appearance: none;   // сброс дефолтных стилей бегунка
-        width: 7.5rem;
-        height: 0.125rem;
+// стили трека
+.custom_slider {
+    appearance: none; // сброс дефолтных стилей бегунка
+    width: 7.5rem;
+    height: 0.125rem;
+    background-color: #72B3EE;
+    border-radius: 2px;
+
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        width: 1.125rem;
+        height: 1.125rem;
         background-color: #72B3EE;
-        border-radius: 2px;
-        
-        &::-webkit-slider-thumb {
-            -webkit-appearance: none;
-            width: 1.125rem;
-            height: 1.125rem;
-            background-color: #72B3EE;
-            border: none;
-            border-radius: 50%;
-            position: relative;
-        }
+        border: none;
+        border-radius: 50%;
+        position: relative;
     }
+}
 </style>
