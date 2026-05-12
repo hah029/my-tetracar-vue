5fae6e8e734e4cb17a92137cd6cbc4050e090359
Roman <roma.chekalin@gmail.com>
2026-05-12 16:28:21 +0300

    build v0.0.14

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,8 +1,8 @@
 // Этот файл генерируется автоматически. Не редактировать вручную!
 
-export const APP_VERSION = '0.0.13';
-export const BUILD_TIME = '2026-05-09T20:31:33.148Z';
-export const BUILD_TIMESTAMP = 1778358693148;
+export const APP_VERSION = '0.0.14';
+export const BUILD_TIME = '2026-05-12T13:26:41.927Z';
+export const BUILD_TIMESTAMP = 1778592401927;
 
 export const VERSION_INFO = {
   version: APP_VERSION,
