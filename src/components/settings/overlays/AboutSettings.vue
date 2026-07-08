7e82116aabf5ba801221c39328c83a61f6247e85
Tikhomolov, Konstantin <k3incho@inbox.ru>
2026-07-08 15:48:26 +0300

    Адаптивная верстка экрана Об игре

--- src/components/settings/overlays/AboutSettings.vue
+++ src/components/settings/overlays/AboutSettings.vue
@@ -9,62 +9,85 @@
 
 
 <script setup lang="ts">
-import { onMounted, ref, watch, computed } from "vue";
-import { createNewText } from '@/helpers/functions';
+    import { onMounted, ref, watch, computed } from "vue";
+    import { createNewText } from '@/helpers/functions';
 
-const foo = createNewText();
-const isRowsEnabled = ref(false);
+    const foo = createNewText();
+    const isRowsEnabled = ref(false);
 
-const aboutButtons = computed(() => [
-    // { id: 1, text: foo.makeText("settings.about.eula"), action: goToEula },
-    { id: 1, text: foo.makeText("settings.about.eula"), action: null },
-    { id: 2, text: foo.makeText("settings.about.thirdParty"), action: null },
-    { id: 3, text: foo.makeText("settings.about.aboutUs"), action: null },
-]);
+    const aboutButtons = computed(() => [
+        // { id: 1, text: foo.makeText("settings.about.eula"), action: goToEula },
+        { id: 1, text: foo.makeText("settings.about.eula"), action: null },
+        { id: 2, text: foo.makeText("settings.about.thirdParty"), action: null },
+        { id: 3, text: foo.makeText("settings.about.howToPlay"), action: null },
+        { id: 4, text: foo.makeText("settings.about.aboutUs"), action: null },
+    ]);
 
-// function goToEula() {
-//     gameStore.setState(GameStates.Countdown);
-// };
+    // function goToEula() {
+    //     gameStore.setState(GameStates.Countdown);
+    // };
 
-// function goToSettings() {
-//     isRowsEnabled.value = false;
-//     setTimeout(() => {
-//         gameStore.openSettings();
-//     }, 300);
-// };
+    // function goToSettings() {
+    //     isRowsEnabled.value = false;
+    //     setTimeout(() => {
+    //         gameStore.openSettings();
+    //     }, 300);
+    // };
 
-const props = defineProps<{
-    backStatus: boolean;
-}>();
+    const props = defineProps<{
+        backStatus: boolean;
+    }>();
 
-watch(() => props.backStatus, (newVal) => {
-    if (newVal) {
-        isRowsEnabled.value = false;
-    };
-});
+    watch(() => props.backStatus, (newVal) => {
+        if (newVal) {
+            isRowsEnabled.value = false;
+        };
+    });
 
-onMounted(() => {
-    setTimeout(() => {
-        isRowsEnabled.value = true;
-    }, 400);
-});
+    onMounted(() => {
+        setTimeout(() => {
+            isRowsEnabled.value = true;
+        }, 750);
+    });
 </script>
 
 
 <style lang="scss" scoped>
-@use "@/styles/menu.scss";
-@use "@/styles/animations.scss";
+    @use "@/styles/menu.scss";
+    @use "@/styles/animations.scss";
+    @use "@/styles/typography" as *;
+    @use "@/styles/colors" as *;
 
-.group_correction {
-    position: static !important;
-    width: max-content;
+    .group_correction {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        width: max-content;
 
-    &>*+* {
-        margin-top: 0.9375rem; // 15px - row-gap (между кнопками)
+        gap: 3.846vh;   // позже расчитать (для мини-мобил)
+
+        @media (min-width: $breakpoint-mobile) and (orientation: landscape) { 
+            gap: 3.846vh;
+        }
+        // позже расчитать:
+        // @media (min-width: $breakpoint-tablet) and (orientation: landscape) { 
+        //     bottom: 12.8125rem;
+        //     gap: 5.56vh; 
+        //     justify-content: center !important;
+        // }  
+        // @media (min-width: $breakpoint-laptop) and (orientation: landscape) { 
+        //     bottom: 12.8125rem;
+        //     gap: 5.56vh; 
+        //     justify-content: center !important;
+        // }
+        @media (min-width: $breakpoint-desktop) and (orientation: landscape) {
+            gap: 1.125rem;
+        }
     }
-}
 
-.btn_correction {
-    font-size: min(1.875rem, 22px);
-}
+    .btn_correction {
+        @include text-secondary-menu-button;
+        color: $color-yellow-super-light;
+    }
 </style>
