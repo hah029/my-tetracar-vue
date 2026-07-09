6282c462da0e3cc46b48538ff8a7d4a7993a02b5
Tikhomolov, Konstantin <k3incho@inbox.ru>
2026-07-09 10:27:50 +0300

    Адаптивная верстка экрана Графика

--- src/components/settings/overlays/GraphicSettings.vue
+++ src/components/settings/overlays/GraphicSettings.vue
@@ -3,24 +3,16 @@
         <!-- Bloom -->
         <div v-if="rowView[1]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.bloomEnabled", "Bloom") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.bloomEnabled }"
-                @click="toggleBloom">
-                {{ graphicsStore.bloomEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }}
+            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.bloomEnabled }" @click="toggleBloom">
+                {{ defineSwitcherState(graphicsStore.bloomEnabled) }}
             </button>
         </div>
 
         <!-- Bloom -->
         <div v-if="rowView[2]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.afterimageEnabled", "Motion Blur") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.afterimageEnabled }"
-                @click="toggleMotionBlur">
-                {{ graphicsStore.afterimageEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }}
+            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.afterimageEnabled }" @click="toggleMotionBlur">
+                {{ defineSwitcherState(graphicsStore.afterimageEnabled) }}
             </button>
         </div>
 
@@ -28,48 +20,25 @@
         <div v-if="rowView[3]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.fxaaEnabled", "FXAA") }}</span>
             <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.fxaaEnabled }" @click="toggleFxaa">
-                {{ graphicsStore.fxaaEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }}
+                {{ defineSwitcherState(graphicsStore.fxaaEnabled) }}
             </button>
         </div>
 
         <!-- RGB смещение -->
         <div v-if="rowView[4]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.rgbShiftEnabled", "RGB Shift") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.rgbShiftEnabled }"
-                @click="toggleRGBShift">
-                {{ graphicsStore.rgbShiftEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }}
+            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.rgbShiftEnabled }" @click="toggleRGBShift">
+                {{ defineSwitcherState(graphicsStore.rgbShiftEnabled) }}
             </button>
         </div>
 
         <!-- Тени -->
         <div v-if="rowView[5]" class="settings_row">
             <span>{{ foo.makeText("settings.vfxAndMusic.shadowEnabled", "Тени") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.shadowEnabled }"
-                @click="toggleShadows">
-                {{ shadowLabel }} 1
-                <!-- {{ graphicsStore.shadowEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }} -->
+            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.shadowEnabled }" @click="toggleShadows">
+                {{ shadowLabel }}
             </button>
         </div>
-
-        <!-- <div v-if="rowView[5]" class="settings_row">
-            <span>{{ foo.makeText("settings.vfxAndMusic.shadowEnabled", "Тени") }}</span>
-            <button class="toggle_btn" :class="{ 'toggle_btn--active': graphicsStore.shadowEnabled }"
-                @click="toggleShadows">
-                {{ graphicsStore.shadowEnabled ?
-                    foo.makeText("settings.toggleOn", "empty") :
-                    foo.makeText("settings.toggleOff", "empty")
-                }}
-            </button>
-        </div> -->
     </TransitionGroup>
 </template>
 
@@ -81,35 +50,30 @@
 
     const graphicsStore = useGraphicsStore();
     const rowView = ref(Array(7).fill(false));
-
     const foo = createNewText();
     const shadowLabel = ref('');
 
-
-    // graphicsStore.shadowEnabled ?
-    //     foo.makeText("settings.toggleOn", "empty") :
-    //     foo.makeText("settings.toggleOff", "empty")
-    
-
-
-
-
+    // вкл / выкл свечения (Bloom)
     function toggleBloom() {
         graphicsStore.toggleBloom();
     };
 
+    // вкл / выкл размытия при движении (Motion blur)
     function toggleMotionBlur() {
         graphicsStore.toggleMotionBlur();
     };
 
+    // вкл / выкл сглаживания (FXAA - Fast Approximate Anti-Aliasing)
     function toggleFxaa() {
         graphicsStore.toggleFxaa();
     };
 
+    // вкл / выкл хроматических аберраций (RGBShift или Chromatic Aberration)
     function toggleRGBShift() {
         graphicsStore.toggleRGBShift();
     };
 
+    // вкл / выкл теней (4 состояния: выкл, низкие, средние, высокие)
     function toggleShadows() {
         if (!graphicsStore.shadowEnabled) {
             graphicsStore.toggleShadow();
@@ -130,6 +94,13 @@
         };
     };
 
+    // переключение между двумя состояниями свитчера: вкл / выкл
+    function defineSwitcherState(statement_) {
+        return statement_ ? 
+            foo.makeText("settings.toggleOn", "empty") : 
+            foo.makeText("settings.toggleOff", "empty")
+    };
+
     const props = defineProps<{
         backStatus: boolean;
     }>();
@@ -147,12 +118,24 @@
     });
 
     onMounted(() => {
+        if (!graphicsStore.shadowEnabled) {
+            shadowLabel.value = foo.makeText("settings.toggleOff", 'empty');
+        } else {
+            if (graphicsStore.shadowQuality === "low") {
+                shadowLabel.value = foo.makeText("settings.low", 'empty');
+            } else if (graphicsStore.shadowQuality === "medium") {
+                shadowLabel.value = foo.makeText("settings.medium", 'empty');
+            } else {
+                shadowLabel.value = foo.makeText("settings.high", 'empty');
+            };
+        };
+
         // Появление с задержками
         const total = rowView.value.length;
         for (let i = 0; i < total; i++) {
             setTimeout(() => {
                 rowView.value[i] = true;
-            }, 750 + i * 100); // начинаем с задержки 400 мс
+            }, 750 + i * 100);
         };
     });
 </script>
