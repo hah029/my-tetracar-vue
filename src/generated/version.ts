79e84afa13c34ef07d93c8d60465b457fdcbf548
Roman <roma.chekalin@gmail.com>
2026-08-14 18:05:28 +0300

    upd version to 0.0.38

--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,8 +1,8 @@
 // Этот файл генерируется автоматически. Не редактировать вручную!
 
-export const APP_VERSION = '0.0.37';
-export const BUILD_TIME = '2026-07-27T14:57:48.262Z';
-export const BUILD_TIMESTAMP = 1785164268263;
+export const APP_VERSION = '0.0.38';
+export const BUILD_TIME = '2026-08-14T13:47:33.939Z';
+export const BUILD_TIMESTAMP = 1786715253940;
 
 export const VERSION_INFO = {
   version: APP_VERSION,
